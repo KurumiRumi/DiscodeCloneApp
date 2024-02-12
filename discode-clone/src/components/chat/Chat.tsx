d3a599f1679313d16c217f7dfc62eb297a166ee5
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 14:17:17 +0900

    20. チャットヘッダーを作成してみよう

--- discode-clone/src/components/chat/Chat.tsx
+++ discode-clone/src/components/chat/Chat.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
 
 const Chat = () => {
   return (
     <div className="chat">
       {/* chatHeader */}
+      <ChatHeader />
       {/* chatMessage */}
+      <div className="chatMessage"></div>
       {/* chatInput */}
+      <div className="chatInput"></div>
     </div>
   );
 };
