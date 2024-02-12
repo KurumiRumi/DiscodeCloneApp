151e6597713364a57ec165fd8234b4f47267447b
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 12:56:57 +0900

    13. MaterialUIをインストールしてアイコンを利用してみよう

--- discode-clone/src/components/sidebar/Sidebar.tsx
+++ discode-clone/src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import "./Sidebar.scss";
 
@@ -18,6 +19,7 @@ const Sidebar = () => {
       <div className="sidebarRight">
         <div className="sidebarTop">
           <h3>Discode</h3>
+          <ExpandMoreIcon />
         </div>
       </div>
     </div>
