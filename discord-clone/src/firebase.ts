6832d08dddc62424f7d9a61aed20fd931c1b278e
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 15:41:40 +0900

    27. Firebaseの初期設定をはじめよう

---
+++ discord-clone/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { firebaseConfig } from "./firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider };
