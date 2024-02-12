18673efa1822bfc22dd8b51bad0b3075f50a32ec
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 13:45:35 +0900

    16. サイドバーのフッターアカウントを作成してみよう

--- discode-clone/src/components/sidebar/Sidebar.tsx
+++ discode-clone/src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
 
 import "./Sidebar.scss";
 import SidebarChannel from "./SidebarChannel";
@@ -39,6 +42,22 @@ const Sidebar = () => {
             <SidebarChannel />
             <SidebarChannel />
             <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./logo192.png" alt="" />
+              <div className="accountName">
+                <h4>TestUser</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
