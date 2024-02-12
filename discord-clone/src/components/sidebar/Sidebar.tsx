b6df2fcbcbb7810b037382a57e187232a069aa33
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 19:01:21 +0900

    45. Discordチャンネルを追加する実装をしてみよう

--- discord-clone/src/components/sidebar/Sidebar.tsx
+++ discord-clone/src/components/sidebar/Sidebar.tsx
@@ -7,13 +7,24 @@ import HeadphonesIcon from "@mui/icons-material/Headphones";
 
 import "./Sidebar.scss";
 import SidebarChannel from "./SidebarChannel";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { useAppSelector } from "../../app/hooks";
 import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
 
 const Sidebar = () => {
   const user = useAppSelector((state) => state.user);
   const { documents: channels } = useCollection("channels");
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt("新しいチャンネルを作成します");
+
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -41,7 +52,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>プログラミングチャンネル</h4>
             </div>
-            <AddIcon className="sidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()} />
           </div>
 
           <div className="sidebarChannelList">
