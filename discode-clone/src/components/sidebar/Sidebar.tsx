59b1b2570dd538caba85e9fcf976f2ba61698832
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-12 12:36:15 +0900

    11. サイドバーの中身のHTMLとCSSの記述をはじめよう

--- discode-clone/src/components/sidebar/Sidebar.tsx
+++ discode-clone/src/components/sidebar/Sidebar.tsx
@@ -6,9 +6,20 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
-      <div className="sidebarLeft"></div>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
       {/* sidebarRight */}
-      <div className="sidebarRight"></div>
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discode</h3>
+        </div>
+      </div>
     </div>
   );
 };
